edfad1be8a3f5be6b8ecee9a3c9a1fca090bc22d
Shinderohit1822 <115020974+Shinderohit1822@users.noreply.github.com>
2022-10-04 20:27:41 +0530

    Create first.py

---
+++ first.py
@@ -0,0 +1,37 @@
+
+
+v1 = {1:100,2:200}
+print('Before setdefault---')
+
+print(v1)
+
+x1 = v1.setdefault(1,111)
+x2 = v1.setdefault(3,300)
+
+
+v1 = {1:100,2:200,3:300,'A':500,4:231}
+x1 = v1.values()
+x2 = v1.keys()
+x3 = v1.items()
+print(v1)
+v1.popitem()
+
+v1[44] = 400
+print(v1
+    )
+
+import sys
+sys.exit(0)
+
+
+
+
+
+
+
+
+
+print(v1)
+print(dir(v1))
+
+print(v1[1])
